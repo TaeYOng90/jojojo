592a52745a2352b66ba2552f741fc7ca50dbc5ac
taeyong <taeyong@def.com>
2022-08-09 17:54:52 +0900

    hi

---
+++ jojojo/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='main1'>
+        <h2>oh Dog</h2>
+      </div>
+
+
+      <div className='menu'>
+        <div className='menu-s'>나의 피드</div>
+        <div className='menu-s'>사진첩</div>
+        <div className='menu-s'>캘린더</div>
+      </div>
+
+
+
+      <div className='main-box'>
+        <div className='l-box'></div>
+        <div className='s-box'></div>
+      </div>
+
+
+
+    </div>);
+}
+
+export default App;
